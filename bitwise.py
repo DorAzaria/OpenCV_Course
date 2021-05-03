c0ab89dbd61836c54737bbb9f50ee7901154e37a
Dor Azaria <46644036+DorAzaria@users.noreply.github.com>
2021-05-03 21:33:12 +0300

    OpenCV 2

---
+++ bitwise.py
@@ -0,0 +1,27 @@
+import cv2 as cv
+import numpy as np
+
+blank = np.zeros((400,400), dtype='uint8')
+
+rectangle = cv.rectangle(blank.copy(), (30,30), (370,370), 255, -1)
+circle = cv.circle(blank.copy(), (200,200), 200, 255, -1)
+cv.imshow('Rectangle', rectangle)
+cv.imshow('Circle', circle)
+
+# Bitwise AND
+bitwise_and = cv.bitwise_and(rectangle, circle)
+cv.imshow('Rectangle AND Circle', bitwise_and)
+
+# Bitwise OR
+bitwise_or = cv.bitwise_or(rectangle,circle)
+cv.imshow('Rectangle OR Circle', bitwise_or)
+
+# Bitwise XOR
+bitwise_xor = cv.bitwise_xor(rectangle, circle)
+cv.imshow('Rectangle XOR Circle', bitwise_xor)
+
+# Bitwise NOT
+bitwise_not = cv.bitwise_not(rectangle)
+cv.imshow('Rectangle NOT', bitwise_not)
+
+cv.waitKey(0)
--- smoothing.py
+++ smoothing.py
@@ -3,5 +3,20 @@ import cv2 as cv
 img = cv.imread('Photos/cats.jpg')
 cv.imshow('Cats', img)
 
-# 01:32:30
+# Averaging
+average = cv.blur(img, (3,3))
+cv.imshow('Average Blur', average)
+
+# Gaussian Blur
+gauss = cv.GaussianBlur(img, (3,3), 0)
+cv.imshow('Gaussian Blur', gauss)
+
+# Median Blur
+median = cv.medianBlur(img, 3)
+cv.imshow('Median', median)
+
+# Bilateral Blur
+bilateral = cv.bilateralFilter(img, 3, 15, 15)
+cv.imshow('Bilateral', bilateral)
+
 cv.waitKey(0)
